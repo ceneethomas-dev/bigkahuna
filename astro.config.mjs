482874f9f9d8d0df414915597f65e5c0c05e7879
ceneethomas-dev <cenee.thomas@icloud.com>
2026-08-06 22:41:11 +0200

    ajout JsonLd

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-	site: 'https://bigkahuna-pearl.vercel.app',
+  site: 'https://bigkahuna-pearl.vercel.app',
+  integrations: [sitemap()],
 });
