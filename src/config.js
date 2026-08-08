b605d84eb051f64ae829c306b74d806ec966d9ee
ceneethomas-dev <cenee.thomas@icloud.com>
2026-08-08 22:20:05 +0200

    ajout fichier de configuration

---
+++ src/config.js
@@ -0,0 +1,22 @@
+export const siteConfig = {
+	name: "Mon Restaurant",
+	description: "Restaurant traditionnel proposant une cuisine faite maison, dans une ambiance chaleureuse.",
+	address: {
+		street: "12 rue de la Paix",
+		city: "Pau",
+		postalCode: "64000",
+		country: "FR"
+	},
+	phone: "+33-5-XX-XX-XX-XX",
+	cuisine: "Française",
+	priceRange: "€€",
+	openingHours: {
+		days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+		opens: "12:00",
+		closes: "22:00"
+	},
+	social: {
+		facebook: "https://facebook.com/monrestaurant",
+		instagram: "https://instagram.com/monrestaurant"
+	}
+};
