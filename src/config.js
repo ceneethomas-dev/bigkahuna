99afed321c2eaf188f2cbd46c23b48fddd6e3d79
ceneethomas-dev <cenee.thomas@icloud.com>
2026-08-09 13:40:24 +0200

    systeme horaire

--- src/config.js
+++ src/config.js
@@ -1,20 +1,24 @@
 export const siteConfig = {
-	name: "Big Kahuna",
+	name: "Mon Restaurant",
 	description: "Restaurant traditionnel proposant une cuisine faite maison, dans une ambiance chaleureuse.",
 	address: {
-		street: "20 rue de Satory",
-		city: "Versailles",
-		postalCode: "78000",
+		street: "12 rue de la Paix",
+		city: "Pau",
+		postalCode: "64000",
 		country: "FR"
 	},
-	phone: "01 23 45 67 89",
+	phone: "+33-5-XX-XX-XX-XX",
 	cuisine: "Française",
 	priceRange: "€€",
-	openingHours: {
-		days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
-		opens: "12:00",
-		closes: "22:00"
-	},
+	openingHours: [
+		{ day: "Monday", label: "Lundi", closed: true, slots: [] },
+		{ day: "Tuesday", label: "Mardi", slots: [{ opens: "12:00", closes: "14:00" }, { opens: "19:00", closes: "22:00" }] },
+		{ day: "Wednesday", label: "Mercredi", slots: [{ opens: "12:00", closes: "14:00" }, { opens: "19:00", closes: "22:00" }] },
+		{ day: "Thursday", label: "Jeudi", slots: [{ opens: "12:00", closes: "14:00" }, { opens: "19:00", closes: "22:00" }] },
+		{ day: "Friday", label: "Vendredi", slots: [{ opens: "12:00", closes: "14:30" }, { opens: "19:00", closes: "23:00" }] },
+		{ day: "Saturday", label: "Samedi", slots: [{ opens: "19:00", closes: "23:00" }] },
+		{ day: "Sunday", label: "Dimanche", closed: true, slots: [] }
+	],
 	social: {
 		facebook: "https://facebook.com/monrestaurant",
 		instagram: "https://instagram.com/monrestaurant"
